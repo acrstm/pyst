0ba1c2ed5e558960699e0f8252069df23f0af97b
Alam Sena <alam@koarquitetos.com>
2023-06-01 20:37:29 +0200

    group photo upload

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,5 @@
 class GroupsController < ApplicationController
 
-
   def index
     @groups = Group.all
   end
@@ -9,31 +8,32 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
-
-
   def create
     @group = Group.new(group_params)
 
     @user = current_user
-
     # @task.user = @user
 
     @group.save
     @user.group_id = @group.id
     @user.save
-    redirect_to groups_path(@group)
+    redirect_to group_path(@group)
   end
 
-
-
-private
-
-def group_params
-  params.require(:group).permit(:name, :description, :avatar)
-end
+  def show
+    @group = Group.find(params[:id])
+    @photo = @group.photo.key
+  end
 
   def join
 
+  end
+
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :description, :photo)
   end
 
 end
