7fcda2d2731579543c90281cb831836e86bc2eff
Alam Sena <alam@koarquitetos.com>
2023-06-09 20:14:12 +0200

    searchbar, tasks index improov

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,13 @@ class Task < ApplicationRecord
   default_scope -> { order('created_at DESC') }
   scope :completed, -> { where(completed: true) }
   scope :incomplete,-> { where(completed: false) }
+
+
+  include PgSearch::Model
+  pg_search_scope :search_by_task_and_name,
+    against: [ :name, :assigned_to_id ],
+    using: {
+      tsearch: { prefix: true }, # <-- now `superman batm` will return something!
+}
+
 end
